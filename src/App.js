682ddcea6809b837013f77dd7ef7935e8e080c60
Michael Petrov <mpetrovcello@gmail.com>
2021-01-26 15:26:56 +0000

    create first react app skeleton

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> We are on React baby!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
